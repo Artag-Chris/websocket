daf9053e05b60ee62914b15741aa35b616928a11
Artag-Chris <scristxyz@gmail.com>
2024-08-17 11:31:42 -0500

    terminando aplicacion de mensajes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, WebSocket } from 'ws';
 
 const wss = new WebSocketServer({ port: 3000 });
 
@@ -8,13 +8,36 @@ wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
 
   ws.on('message', function message(data) {
-    console.log('received: %s', data);
+
+    //console.log('received: %s', data);
+    const payload = JSON.stringify({
+      type: "custom-message",
+      payload: data.toString()
+    });
+
+   // ws.send(JSON.stringify(payload));
+   wss.clients.forEach( function each(client){
+    //para mostarle a todos incluido
+    // if(client.readyState === WebSocket.OPEN){
+    //   client.send(payload,{binary:false});
+    // }
+
+    //para mostrarle a todos excluido
+    if(client !== ws && client.readyState === WebSocket.OPEN){
+      client.send(payload,{binary:false});
+    }
+
+   })
   });
 
-  ws.send('hola desde web sockets');
+  ws.on('close', function close() {
+    console.log('se desconecto alguien');
+  });
 
-  setInterval(() => {
-    ws.send('hola cada 2 segundos');
-  },2000)
+ // ws.send('hola desde web sockets');
+
+  // setInterval(() => {
+  //   ws.send('hola cada 2 segundos');
+  // },2000)
 });
  console.log('servidor iniciado localhost:3000');
